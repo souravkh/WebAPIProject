bf0b9e414eb08a1a47664cd14e1daba41af5efb9
souravkh <souravkhatri1996@gmail.com>
2025-04-26 23:32:06 +0530

    Add database info

---
+++ src/EmployeeCrudAPI/EmployeeCrudAPI/Data/EmployeeDbDataContext.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeCrudAPI.Model.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCrudAPI.Data
+{
+    /// <summary>
+    /// Represents the database context for managing employees.
+    /// Inherits from DbContext to interact with the database.
+    /// </summary>
+    public class EmployeeDbDataContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmployeeDbDataContext"/> class.
+        /// </summary>
+        /// <param name="dbContextOptions">Options to configure the database context.</param>
+        public EmployeeDbDataContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="DbSet{Employee}"/> representing the Employees table in the database.
+        /// </summary>
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- src/EmployeeCrudAPI/EmployeeCrudAPI/Program.cs
+++ src/EmployeeCrudAPI/EmployeeCrudAPI/Program.cs
@@ -1,9 +1,13 @@
+using EmployeeCrudAPI.Data;
 using EmployeeCrudAPI.Mapping;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<EmployeeDbDataContext>(options =>
+options.UseSqlServer(connectionString:
+builder.Configuration.GetConnectionString("EmployeeDbConnectionString")));
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 var app = builder.Build();
